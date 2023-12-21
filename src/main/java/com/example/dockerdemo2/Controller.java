4ba596adadfc6f5a4a16f7a22715fd2b1f4c187f
Michael Glazier <mglazier@laptopemt.com>
2023-12-21 10:39:11 -0600

    First Push

---
+++ src/main/java/com/example/dockerdemo2/Controller.java
@@ -0,0 +1,14 @@
+package com.example.dockerdemo2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/api/v1/message")
+    public String getMessage()
+    {
+        return "Hello World from Spring Boot";
+    }
+}
